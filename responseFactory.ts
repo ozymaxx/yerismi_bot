4c2613982b53baef21a74135b9e25caba1afca4c
Ozan Can Altıok <ozymaxx@gmail.com>
2019-10-01 23:01:26 +0300

    add support for generating fake proverbs

--- responseFactory.ts
+++ responseFactory.ts
@@ -23,15 +23,20 @@ export class ResponseFactory
                 placeNameCommand: Constants.PLACE_NAME_COMMAND,
                 proverbCommand: Constants.PROVERB_COMMAND
             });
+    private static readonly PLACENAME_MODEL_FILE_PATH = "cv/checkpoint_7600.t7";
+    private static readonly PROVERB_MODEL_FILE_PATH = "cv/checkpoint_1600.t7";
+    private static readonly PLACENAME_CHAR_LENGTH = 7600;
+    private static readonly PROVERB_CHAR_LENGTH = 1600;
+    private static readonly COMMAND_TEMPLATE = "th sample.lua -checkpoint %(modelFilePath)s -length %(charLength)d -gpu -1";
     private static readonly INVALID_COMMAND_RESPONSE = 
         sprintf(
             "Ne istediğini anlamadım; '%(helpCommand)s' yazarak yardım isteyebilirsin.", 
             { helpCommand: Constants.HELP_COMMAND });
 
-    private static createFakePlacename(): string
+    private static createFakeResponse(charLength: number, modelFilePath: string): string
     {
         const outputWithMultipleLines = 
-            exec("th sample.lua -checkpoint cv/checkpoint_7600.t7 -length 100 -gpu -1").stdout.trim();
+            exec(sprintf(ResponseFactory.COMMAND_TEMPLATE, { charLength, modelFilePath })).stdout.trim();
         const splitOutput = outputWithMultipleLines.split("\n");
         return splitOutput[2];
     }
@@ -43,9 +48,11 @@ export class ResponseFactory
             case Constants.HELP_COMMAND:
                 return ResponseFactory.HELP_STRING;
             case Constants.PROVERB_COMMAND:
-                return exec("echo 'ozan'").stdout;
+                return ResponseFactory.createFakeResponse(
+                    ResponseFactory.PROVERB_CHAR_LENGTH, ResponseFactory.PROVERB_MODEL_FILE_PATH);
             case Constants.PLACE_NAME_COMMAND:
-                return ResponseFactory.createFakePlacename();
+                return ResponseFactory.createFakeResponse(
+                    ResponseFactory.PLACENAME_CHAR_LENGTH, ResponseFactory.PLACENAME_MODEL_FILE_PATH);
             default:
                 return ResponseFactory.INVALID_COMMAND_RESPONSE;
         }
